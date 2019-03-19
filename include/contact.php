42691784243284e3e0832bd031479d6303e65a81
JoneCoder <mohummadshoriful@gmail.com>
2019-03-19 11:11:06 +0600

    update resume

---
+++ include/contact.php
@@ -0,0 +1,46 @@
+<!-- introduce -->
+<div class="confi-w3l">
+    <div class="container">
+        <h3>Hello....</h3>
+        <p>It's my pleasure to introduce my self..well, I'm Sharif and raised in Bangladesh.</p>
+        <p>My strengths are my attitude that i like to take challenges that I CAN do it,my way of thinking that i take both success and failure in a balanced manner..</p>
+    </div>
+</div>
+<!-- //introduce -->
+<!-- //contact -->
+<div class="address" id="contact">
+    <div class="container">
+        <h3 class="w3l-title">Contact Me</h3>
+        <div class="address-row">
+            <div class="col-md-6 address-left wow agile fadeInLeft animated" data-wow-delay=".5s">
+                <div class="address-grid">
+                    <h4 class="wow fadeIndown animated" data-wow-delay=".5s">Get In Touch</h4>
+                    <form action="#" method="post">
+                        <input type="text" placeholder="Name" name="name" required="">
+                        <input type="email" placeholder="Email" name="email" required="">
+                        <input type="text" placeholder="Subject" name="subject" required="">
+                        <textarea placeholder="Message" required=""></textarea>
+                        <input type="submit" value="SEND">
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6 address-right">
+                <div class="address-info wow fadeInRight animated" data-wow-delay=".5s">
+                    <h4>Address</h4>
+                    <p>93, Narinda Road, Dhaka-Bangladesh.</p>
+                </div>
+                <div class="address-info address-mdl wow fadeInRight animated" data-wow-delay=".7s">
+                    <h4>Phone </h4>
+                    <p>+880 171 207 4151</p>
+                    <p>+880 184 201 5935</p>
+                </div>
+                <div class="address-info agileits-info wow fadeInRight animated" data-wow-delay=".6s">
+                    <h4>Mail</h4>
+                    <p><a href="mohummadsharif4@gmail.com"> mohummadsharif4@gmail.com</a></p>
+                    <p><a href="mohummadshorifulislam@gmail.com"> mohummadshorifulislam@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--//contact-->
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'include/head.php';
+include 'include/banner.php';
+include 'include/about.php';
+include 'include/stats.php';
+include 'include/services.php';
+include 'include/interest.php';
+include 'include/experience.php';
+include 'include/education.php';
+include 'include/projects.php';
+include 'include/contact.php';
+require 'include/footer.php';
+?>
